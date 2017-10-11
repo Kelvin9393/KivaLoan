7f03d154699933fe65af27ff70c2659c870e5c3e
Kelvin9393 <kelvinlssiang9393@hotmail.com>
2017-10-11 13:00:15 +0800

    first commit

---
+++ KivaLoan/Loan.swift
@@ -0,0 +1,16 @@
+//
+//  Loan.swift
+//  KivaLoan
+//
+//  Created by KELVIN LING SHENG SIANG on 11/09/2017.
+//  Copyright © 2017 AppCoda. All rights reserved.
+//
+
+import Foundation
+
+class Loan {
+    var name: String = ""
+    var country: String = ""
+    var use: String = ""
+    var amount: Int = 0
+}
